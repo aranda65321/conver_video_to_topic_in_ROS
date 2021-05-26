93c4509366c09d6486f39dfe3478d0fabb90f8c2
aranda65321 <72268235+aranda65321@users.noreply.github.com>
2021-05-25 21:06:56 -0500

    Update convert.py

--- convert.py
+++ convert.py
@@ -10,30 +10,30 @@ import time
 
 
 def start_node():
-#iniciamos un nodo llamado image_pub
+#we start a node called image_pub
 	rospy.init_node('image_pub')
-#escribimos un mensaje informado que se ha iniciado el nodo
+#we write a message informed that the node has started
 	rospy.loginfo('image_pub nodo iniciado')
-#instanciamos al metodo CbBridge
+#instance the CbBridge function
 	bridge = CvBridge() 
-#cargamos el video en este caso el video esta en una carpeta  del paquete
-#el usuario debe cambiar esa ruta por la donde tiene su video
+# we load the video in this case the video is in a folder of the package
+#the user must change that path to the one where they have their video
         captura = cv2.VideoCapture('/home/k-milo/catkin_ws/src/p_video/videos/video.mp4')
-#ahora separamos el video en frames, la variable cv_imagen es un frame del video
+#Now we separate the video into frames, the variable cv_imagen is a frame of the video
 	ret, cv_imagen = captura.read()
-#creamos un ciclo while que funciona reproducciendo y avanzando cada frame del video
+# we create a while loop that works by playing and advancing each frame of the video
 	while (True):
 	  ret, cv_imagen = captura.read()
-#si se esta cargando la informacion o frames del video se hace lo siguiente
+#if the information or video frames are being loaded, do the following
 	  if ret == True:
-#mostramos cada frame del video con un imshow
+#show each frame of the video with an imshow
 	    cv2.imshow('video', cv_imagen)
-#convierto cada frame de open_cv a tipo imagen ros
+#convert each frame from open_cv to image type ros
 	    imgMsg = bridge.cv2_to_imgmsg(cv_imagen, "bgr8")
-#publicamos en un topic llamado video_to_topic la imagen convertida, el nombre del topic se puede cambiar
+# we publish the converted image in a topic called video_to_topic, the name of the topic can be changed
 	    pub = rospy.Publisher('video_to_topic', Image)
             pub.publish(imgMsg)
-#se crea un delay entre cada frame de 25 milisegundos
+# a delay between each frame of 25 milliseconds is created
 	    if cv2.waitKey(25) == ord('s'):
 	      break
 	  else: break
@@ -44,7 +44,7 @@ def start_node():
 
 if __name__ == '__main__':
     try:
-#funcion que se inicia con cuando ejecutamos el script, apenas se ejecuta el script llamamos a la funcion start_node que esta arriba
+#function that starts with when we execute the script, as soon as the script is executed we call the start_node function that is above
         start_node()
     except rospy.ROSInterruptException:
         pass
